3022f2cef7c38070824f6437fc0365566f75148f
Vedaad Shakib <vedaad799@gmail.com>
2018-04-06 05:44:52 -0700

    Restructure

--- test/test_solver.py
+++ test/test_solver.py
@@ -1,3 +1,8 @@
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+
 import block
 import player
 import solver
