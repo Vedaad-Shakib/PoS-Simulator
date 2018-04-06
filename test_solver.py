c725921c6c48235a1ac292b053b001ae9c4934ac
Vedaad Shakib <vedaad799@gmail.com>
2018-04-06 03:10:03 -0700

    Update tests

--- test_solver.py
+++ test_solver.py
@@ -19,15 +19,38 @@ def test_chooseValidators():
     count = [0]*len(p)
     s = solver.Solver(p, 10)
 
-    nRounds = 10000
+    N_ROUNDS = 10000
 
-    for i in range(nRounds):
+    for i in range(N_ROUNDS):
         validators = s.chooseValidators()
-        validators = [j.id for j in validators]
         for j in validators:
             count[j] += 1
 
-    expected = [nRounds*s.nValidators*i for i in s.calcPercentStake()]
+    expected = [N_ROUNDS*s.N_VALIDATORS*i for i in s.calcPercentStake()]
+
+    error = 0.1
+
+    for i, j in zip(count[1:], expected[1:]):
+        assert abs((i-j)/j) < error
+
+    assert count[0] == 0
+
+
+def test_chooseProposers():
+    player.Player.id = 0
+    
+    p = [player.Player(0), player.Player(1), player.Player(1), player.Player(2), player.Player(4), player.Player(8), player.Player(16)]
+    count = [0]*len(p)
+    s = solver.Solver(p, 10)
+
+    N_ROUNDS = 10000
+
+    for i in range(N_ROUNDS):
+        prop = s.chooseProposers()
+        for j in prop:
+            count[j] += 1
+
+    expected = [N_ROUNDS*s.N_PROPOSERS*i for i in s.calcPercentStake()]
 
     error = 0.1
 
